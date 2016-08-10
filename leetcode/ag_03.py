bf2dbb011d1ef92e375fd7539f751687144cbd6c
xiaolongnk <xiaolongnk@126.com>
2016-08-10 10:53:12 +0800

    add new note , ingore useless files

---
+++ leetcode/ag_03.py
@@ -0,0 +1,14 @@
+#!/bin/python2.7
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+if __name__ == '__main__':
+    s = Solution()
+    test = "hello nnsdo"
+    print s.lengthOfLongestSubstring(test)
+
