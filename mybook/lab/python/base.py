85ff8e0470fc5d1bfc6d6a4126a761886187df69
ouxiaolong <ouxiaolong@cmcm.com>
2016-09-13 13:57:59 +0800

    add new conf

---
+++ mybook/lab/python/base.py
@@ -0,0 +1,13 @@
+'''
+函数有多个可省略参数，如何给指定的参数传值
+'''
+
+def funx(a = None , b = None, c = None):
+    print "a b c => %s %s %s"%(a , b ,c)
+    pass
+
+if __name__=='__main__':
+    funx(a=2)
+    funx(b=1)
+    funx(c=33)
+    funx(a=1,c=2)
