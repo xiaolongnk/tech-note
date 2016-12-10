1dc5cf5b90d21bae162cc476dcf10df162936057
xiaolongnk <xiaolongnk@126.com>
2016-12-10 12:16:44 +0800

    add cassandra client

--- mybook/lab/php/cassandra/test_connection.php
+++ mybook/lab/php/cassandra/test_connection.php
@@ -1,44 +1,110 @@
 <?php
 class CassandraFactory
 {
-    static private $instance = null;
+    private $session = null;
+    private $keyspace = '';
 
-    private function __construct()
+    private function __construct($keyspace)
     {
-        echo "ini here".PHP_EOL;
-    }
-
-    public static function getInstance()
-    {
-        if(empty($this->instance)){
-            $this->instance = new self();
+        $host = "127.0.0.1";
+        $port = 9042;
+        $cluster= Cassandra::cluster()->withContactPoints($host)->withPort($port)->build();
+        var_dump($cluster);
+        $this->session = $cluster->connect();
+        if(empty($keyspace)) {
+            echo "keyspace should not empty!";
         }
-        return $this->instance;
-    }
+        $this->keyspace = $keyspace;
+        $usekeyspace = "USE $keyspace";
+        $this->session->execute(
+            new Cassandra\SimpleStatement($usekeyspace)
+        );
 
-    public function query()
-    {
-
-    }
-
-    public function insert()
-    {
 
     }
 
-    public function update()
+    public static function getInstance($keyspace)
     {
-        $cluster   = Cassandra::cluster()->build();
-        $keyspace  = 'test';
-        $session   = $cluster->connect($keyspace);
-        $statement = new Cassandra\SimpleStatement( 'SELECT * from test');
-        $future    = $session->executeAsync($statement);
-        $result    = $future->get();
-        foreach ($result as $row) {
-            var_dump($row);
+        static $instance = null;
+        if(empty($instance)){
+            $instance = new self($keyspace);
         }
+        return $instance;
+    }
 
+
+    public function changeKeySpace($keyspace)
+    {
+        $this->keyspace = $keyspace;
+        $usekeyspace = "use $keyspace";
+        $ret = $this->session->execute($usekeyspace);
+        return $ret;
+    }
+
+    private function execute($cql , $bind_data)
+    {
+        try {
+            $ret_insert_cql = $this->session->execute(
+                new Cassandra\SimpleStatement($cql),
+                new Cassandra\ExecutionOptions(['arguments' => $bind_data])
+            );
+            return $ret_insert_cql;
+        }catch(\Exception $e){
+            var_dump($e->getMessage());
+            var_dump($e->getCode());
+            echo "exception in insert";
+            return -1;
+        }
+    }
+
+    public function insert($cql  , $bind_data)
+    {
+        return $this->execute($cql , $bind_data);
+    }
+
+    public function query($cql , $bind_data)
+    {
+        return $this->execute($cql , $bind_data);
+    }
+
+    public function update($cql , $bind_data)
+    {
+        return $this->execute($cql , $bind_data);
+    }
+    public function __destruct()
+    {
+        $this->session->close();
+        echo "destoryed";
     }
 }
-$p = CassandraFactory::getInstance();
 
+$keyspace = "test";
+$p = CassandraFactory::getInstance($keyspace);
+
+
+$test_insert_cql = "insert into media_source_table (aid , media_source , ext ) values (? , ? , ' ')" ;
+$test_insert_cql_bind_data =[
+    ['111114' , 'facebook'],
+    ['111115' , 'google'],
+    ['111116' , 'twitter'],
+    ['111117' , 'facebook'],
+    ['111118' , 'google']
+];
+
+foreach($test_insert_cql_bind_data as $v){
+    $ret = $p->insert($test_insert_cql , $v);
+    var_dump("ret for insert" ,$ret);
+}
+
+
+//$test_insert_cql_bind_data = ['111112' , 'google'];
+//$test_insert_cql_bind_data = ['111113' , 'facebook'];
+//$test_insert_cql_bind_data = ['111114' , 'twitter'];
+
+//$test_update_cql = "";
+//$test_update_cql_bind_data = [];
+
+
+$test_query_cql = "select media_source from media_source_table where aid = ?";
+$test_query_cql_bind_data = ['111111'];
+$ret_q = $p->query($test_query_cql , $test_query_cql_bind_data);
