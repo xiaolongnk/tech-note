f0e1a5528b1e643940feb2c7e9beeabf09865ce2
xiaolongnk <xiaolongnk@126.com>
2017-01-17 15:16:25 +0800

    code optmize

--- algrom/base2.php
+++ algrom/base2.php
@@ -4,14 +4,14 @@ $a = [5,3];
 $b = [3,5,9];
 
 $c = array_intersect($a , $b);
-if($a == $c || $b == $c){
+if($a == $c || $b == $c) {
     echo "yes\n";
-}else {
+} else {
     echo "false\n";
 }
 
 if( [3,5] == [3,5]){
-    echo "funk\n";
+    echo "equal\n";
 }
 
 ?>
--- algrom/reverse.php
+++ algrom/reverse.php
@@ -1,16 +1,15 @@
 <?php    
 $mb = "I am 欧小龙";
 $ans = mb_substr($mb , 0, 8 ,'utf8');
-var_dump($ans);
 
 function reverse($str)
 {
 	$ll = mb_strlen($str,'utf8');
 	$ans = "";
-	for($i = $ll; $i > 0 ; $i--){
-		$ans.= mb_substr($str, $i-1 , 1 ,'utf8');
+    for($i = $ll; $i > 0 ; $i--){
+        $content = mb_substr($str , $i - 1 , 1 , 'utf8');
+        $ans .= $content;
 	}
 	return $ans;
 }
 echo reverse($mb);
-
