381e5bdee89424751c1ba5e9c746522c131405e8
xiaolongnk <xiaolongnk@126.com>
2017-03-06 20:12:02 +0800

    add some test code

--- mybook/lab/cpplab/tt.cpp
+++ mybook/lab/cpplab/tt.cpp
@@ -1,12 +1,76 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <sstream>
+
 using namespace std;
 
 
+/***
+ * max length of the ads position can display is 34;
+ * min_space between source and taboola is 8 blank space;
+ * if source is longger , then Powerd By can be skipped;
+ * if source is still logger , then Powered By Taboola can be skipped;
+ *
+ * @date: 2017-03-03 18:44
+ ***/
+string get_taboola_source(const string & source)
+{
+    const string full_taboola = "Powered By Taboola";
+    // len is 17 , powerd by can be skipped whose length is 10.
+    const string only_taboola = "Taboola"; // len is 8
+    const string min_space    = "        ";
+    string taboola_source     = "";
+    int cursor = 9 - source.length();
+    int delta = 5;
+    if( cursor > 0 ) {
+        string blank_space = "";
+        for(int j = 0 ; j < (cursor + 4 + delta) ; ++j ) blank_space += " ";
+        taboola_source = source + min_space + blank_space + full_taboola;
+    } else if ( cursor > -11 ) {
+        string blank_space = "";
+        for(int j = 0 ; j < (15 + cursor + delta)   ; ++j ) blank_space += " ";
+        taboola_source = source + min_space + blank_space + only_taboola;
+    } else {
+        return source;
+    }
+    return taboola_source;
+}
+
+
+/***
+ * simple test code for stringstream.
+ * 2017-03-06 17:37
+ */
+
+void test_stringstream()
+{
+    stringstream hello;
+    hello<<"xx"<<"wew"<<123123;
+    string result;
+    hello>>result;
+    cout<<result<<endl;
+
+    hello.clear();
+    hello.str();
+    hello<<"3333"<<"123123"<<"hello world";
+    string ch;
+    hello>>ch;
+    cout<<ch<<endl;
+    cout<<"============="<<endl;
+    cout<<ch.c_str()<<endl;
+}
+
 int main()
 {
-    string a = "11";
-    a = "22";
-    cout<<a<<endl;
+    vector<string> test;
+    for(short i = 0 ; i < 40 ; ++i){
+        string s = "";
+        for(short j = 0; j < i ; ++j){
+            s += "x";
+        }
+        test.emplace_back(s);
+    }
+    test_stringstream();
     return 0;
 }
--- mybook/lab/php/c.php
+++ mybook/lab/php/c.php
@@ -1,9 +1,48 @@
 <?php
 
+/***
+ * max length of the ads position can display is 34;
+ * min_space between source and taboola is 8 blank space;
+ * if source is longger , then Powerd By can be skipped;
+ * if source is still logger , then Powered By Taboola can be skipped;
+ ***/
+function format_string($origin_str)
+{
+    $cst = "Powerd By Taboola";     // len is 17 , powerd by can be skipped whose length is 10.
+    $taboola_cst = "Taboola";       // len is 8
+    $min_space = "        ";        // len is 8
+    $final_str = "";
+    $j = 9 - strlen($origin_str) ;  // 9 = 34 - 17 - 8
+    if($j > 0) {
+        $blank_space = "";
+        for($i = 0 ; $i < $j ; ++$i) $blank_space.=" ";
+        $final_str = $origin_str.$min_space.$blank_space.$cst;
+    } else if ($j > -11 ){
+        $blank_space = "";
+        for($i = 0 ; $i < 10 + $j ; $i++) $blank_space.=" ";
+        $final_str = $origin_str.$min_space.$blank_space.$taboola_cst;
+    } else {
+        return $origin_str;
+    }
+    return $final_str;
+}
 
-require_once 'a.php';
-echo $aa."\n";
 
-require_once 'b.php';
 
-echo $aa."\n";
+exit(0);
+// below are test code.
+$list = [];
+for($i = 0 ; $i < 40; ++$i) {
+    $str = "";
+    for($j = 0 ; $j < $i ; ++$j) {
+        $str .="x";
+    }
+    $list[] = $str;
+}
+
+for($i = 0 ; $i < count($list) ; ++$i) {
+    echo format_string($list[$i])."\n";
+}
+
+
+
