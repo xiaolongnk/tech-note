14a725dd5f009b1b4fde8b469ab52779a22306ca
ouxiaolong <ouxiaolong@qudian.com>
2018-10-14 17:41:48 +0800

    feautre[upgrade]: update python2.7 to python3, learn python3.

--- mybook/lab/python/base.py
+++ mybook/lab/python/base.py
@@ -1,10 +1,11 @@
+#!/usr/local/bin/python3
+#coding: utf-8
+
 '''
 函数有多个可省略参数，如何给指定的参数传值
 '''
-
-def funx(a = None , b = None, c = None):
-    print "a b c => %s %s %s"%(a , b ,c)
-    pass
+def funx(a =None , b =None , c = None):
+    print ("a b c => %s %s %s" % (a , b ,c))
 
 if __name__=='__main__':
     funx(a=2)
---
+++ mybook/lab/shell/generateNginxConf.sh
@@ -0,0 +1,55 @@
+#/bin/bash
+ 
+# date: 2018-10-14
+# auth: phenix <xiaolongnk@126.com>
+# generate nginx config for laravel projects.
+
+NGINX_CONF_PATH="/usr/local/etc/nginx/servers/"
+NGINX_TEMPLATE_FILE="./nginx_template.conf"
+
+if [ $# -lt 1 ]
+then
+    echo "usage:./generateNginxConf.sh path"
+    echo "path is necessary, you must specified it."
+    exit 1
+fi
+
+targetPath=$1
+if [ ! -d "$targetPath" ]
+then
+    echo "invalid path $targetPath, please check"
+    exit 2
+fi
+
+#pwd=`pwd`
+#projects=`ls "$targetPath" | sed "s:^:$targetPath/: "`
+for ipath in $*
+do
+    projects=`ls "$ipath"`
+    for i in $projects
+    do
+        confInstance="$i"
+        codePath="$targetPath/$i"
+        outputNginxConfName="$NGINX_CONF_PATH/$i.conf"
+        storagePath="$codePath/storage"
+        bootstrapPath="$codePath/bootstrap"
+        cat $NGINX_TEMPLATE_FILE \
+            | sed "s|CONF_INSTANCE|$confInstance|g" \
+            | sed "s|CODE_PATH|$codePath|g" > $outputNginxConfName
+
+        grep -q "$i" /etc/hosts
+        if [ ! $? ]
+        then
+            echo "127.0.0.1 $i" >> /etc/hosts
+        fi
+        if [ -d "$storagePath" ]
+        then
+            chmod -R 777 "$storagePath"
+        fi
+        if [ -d "$bootstrapPath" ]
+        then
+            chmod -R 777 "$bootstrapPath"
+        fi
+    done
+    echo "process finished [$ipath]"
+done
